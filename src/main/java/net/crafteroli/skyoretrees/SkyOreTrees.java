bd6e7c35039eec92ad28ff44e75672f216aceae3
CrafterOli <olidervincent@gmail.com>
2025-09-23 16:59:16 +0200

    first setup

--- src/main/java/net/crafteroli/skyoretrees/SkyOreTrees.java
+++ src/main/java/net/crafteroli/skyoretrees/SkyOreTrees.java
@@ -1,5 +1,9 @@
 package net.crafteroli.skyoretrees;
 
+import net.crafteroli.skyoretrees.block.ModBlocks;
+import net.crafteroli.skyoretrees.item.ModItems;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -34,6 +38,10 @@ public class SkyOreTrees {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
+
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
@@ -45,6 +53,10 @@ public class SkyOreTrees {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.DIRT_RESIN);
+            event.accept(ModItems.DIRT_ACORN);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
